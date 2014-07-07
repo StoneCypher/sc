99c579c1ee758763ed9701da7f02012dab31647a
John Haugeland <stonecypher@gmail.com>
2014-07-07 08:29:45 -0700

    shim fun_forward/1

--- src/sc_gen_sc_shim.erl
+++ src/sc_gen_sc_shim.erl
@@ -7,7 +7,8 @@
 
 -export([
 
-    arg_list/1
+    arg_list/1,
+    fun_forward/3
 
 ]).
 
@@ -18,3 +19,15 @@
 arg_list(N) ->
 
     sc_list:implode(",", [ "A" ++ integer_to_list(I) || I <- lists:seq(1, N) ]).
+
+
+
+
+
+fun_forward(Mod, Fun, Arity) -> 
+
+    FMod  = atom_to_list(Mod),
+    FName = atom_to_list(Fun),
+    FArgs = arg_list(Arity),
+
+    FName ++ "(" ++ FArgs ++ ") -> " ++ FMod ++ ":" ++ FName ++ "(" ++ FArgs ++ ")".
